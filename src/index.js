c2882e92ae9397810c800bc0a39eabea57e19a31
Vladimir <110772568+Vova4o@users.noreply.github.com>
2023-10-02 20:25:56 +0700

    Upload new files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import Card from './Card';
+import App from './containers/App';
+// import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+// import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
